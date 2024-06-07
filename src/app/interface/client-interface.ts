e86bcdfbda0b13d95cda30805c780728c6ce3943
Faffo96 <fabioscar96@gmail.com>
2024-06-07 15:17:20 +0200

    first commit

---
+++ src/app/interface/client-interface.ts
@@ -0,0 +1,16 @@
+export interface Client {
+    societyName:String,
+    clientType: String,
+    vat: String,
+    email: String,
+    addDate: Date,
+    lastContact: Date,
+    totalSales: Number,
+    pec: String,
+    phoneNumber: Number,
+    contactEmail: String,
+    contactName: String,
+    contactSurname: String,
+    contactPhone: Number,
+    societyLogo: null
+}
